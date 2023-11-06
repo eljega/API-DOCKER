e50bf360d37e4a0af76b0b860a4ed479596c5ff6
eljega <playgames.j.g17@gmail.com>
2023-11-06 15:00:15 -0500

    cuarto commit: solucion de asincronia

--- app/database.py
+++ app/database.py
@@ -1,12 +1,17 @@
-from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "mysql+aiomysql://eljega:12345@db/pruebatecnica"
 
-engine = create_engine(
+# Crear un motor asincrónico.
+engine = create_async_engine(
     SQLALCHEMY_DATABASE_URL
 )
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# La clase de sesión que controla las operaciones de la base de datos ahora debe ser asincrónica.
+AsyncSessionLocal = sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
 
 Base = declarative_base()
--- app/main.py
+++ app/main.py
@@ -1,32 +1,25 @@
 from fastapi import FastAPI, HTTPException, Depends
-from sqlalchemy.orm import Session
-from . import models, schemas, database
-from .database import engine
-from fastapi import FastAPI, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from . import models, schemas
+from .database import AsyncSessionLocal
 import httpx
-
-
-
-models.Base.metadata.create_all(bind=engine)
-
-# Reemplaza 'your_api_key' con tu clave de API real de Dark Sky
-DARKSKY_API_KEY = '209c0a67aamsh03178906980785bp12106cjsnd81bba3eef39'
-DARKSKY_URL = 'https://dark-sky.p.rapidapi.com'
-
 
 app = FastAPI()
 
-# Dependency
-def get_db():
-    db = database.SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+# Deberías inicializar la base de datos de manera asíncrona en otro lugar
+# como en un evento de inicio de FastAPI
+# models.Base.metadata.create_all(bind=engine)
 
+DARKSKY_API_KEY = '209c0a67aamsh03178906980785bp12106cjsnd81bba3eef39'
+DARKSKY_URL = 'https://dark-sky.p.rapidapi.com'
+
+# Dependency
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
 
 @app.get("/api/data")
-async def get_data(latitude: float, longitude: float):
+async def get_data(latitude: float, longitude: float, db: AsyncSession = Depends(get_db)):
     headers = {
         'X-RapidAPI-Key': DARKSKY_API_KEY,
         'X-RapidAPI-Host': "dark-sky.p.rapidapi.com"
@@ -42,16 +35,17 @@ async def get_data(latitude: float, longitude: float):
         return data
 
 @app.post("/api/data", response_model=schemas.Data)
-async def post_data(data: schemas.DataCreate, db: Session = Depends(get_db)):
+async def post_data(data: schemas.DataCreate, db: AsyncSession = Depends(get_db)):
     db_data = models.Data(content=data.content)
     db.add(db_data)
-    db.commit()
-    db.refresh(db_data)
+    await db.commit()
+    await db.refresh(db_data)
     return db_data
 
 @app.get("/api/data/{data_id}", response_model=schemas.Data)
-async def get_data_by_id(data_id: int, db: Session = Depends(get_db)):
-    db_data = db.query(models.Data).filter(models.Data.id == data_id).first()
+async def get_data_by_id(data_id: int, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(models.Data.query.where(models.Data.id == data_id))
+    db_data = result.scalars().first()
     if db_data is None:
         raise HTTPException(status_code=404, detail="Data not found")
     return db_data
