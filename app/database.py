371fcb14b32d46d364eb162929d4816d62a61dbf
eljega <playgames.j.g17@gmail.com>
2023-11-06 10:45:01 -0500

    segundo commit: conexion a base de datos

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+aiomysql://eljega:12345/pruebatecnica"
+SQLALCHEMY_DATABASE_URL = "mysql+aiomysql://eljega:12345@db/pruebatecnica"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
